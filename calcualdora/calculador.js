79f7a17d7cc907332297ed0b19c5c64d8b8ad617
Bryan Antamba <bryjus2003@gmail.com>
2024-10-20 19:43:10 -0500

    nuevo cambio js

---
+++ calcualdora/calculador.js
@@ -0,0 +1,9 @@
+var numero1=prompt("Ingrese el primer numero");
+var numero2=prompt("Ingrese el segundo numero");
+prompt("Ingresa Opcion *1* si es suma, Ingresa *2* si es resta, Ingresa *3* si es multiplicacion. Ingresa *4* si es divicion, Ingresa *5* si es potencia, Ingresa *6* si es calculo de matriz");
+var resultado;
+switch(operacion){
+    case 1:
+        resultado=numero1+numero2
+        document.write("Resultado"+numero1+"+"+numero2+"="+resultado);
+}
